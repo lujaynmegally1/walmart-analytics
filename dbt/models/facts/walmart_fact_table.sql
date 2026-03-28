c9a9476fbafa9762970f16459844a92f523b9890
lujaynmegally1 <97066492+lujaynmegally1@users.noreply.github.com>
2026-03-28 00:48:09 -0700

    Update walmart_fact_table.sql

--- dbt/models/facts/walmart_fact_table.sql
+++ dbt/models/facts/walmart_fact_table.sql
@@ -1,38 +1,31 @@
-{{ config(
-  materialized='incremental',
-  incremental_strategy = 'merge',
-  unique_key= ['store_id', 'dept_id'],
-  database='WALMART',
-  schema='BRONZE',
-  alias='walmart_store_dim'
-) }}
-
+-- Final fact table: reads from SCD2 snapshot, surfaces only current (active) records.
+-- dbt_valid_to IS NULL = the active/current version of each record.
+-- dbt_valid_from / dbt_valid_to are exposed as vrsn_start_date / vrsn_end_date.
 
 {{ config(
-   materialized='table',
-   database='WALMART',
-   schema='BRONZE',
-   alias='walmart_fact_table'
+    materialized = 'table',
+    database = 'WALMART',
+    schema = 'PUBLIC_BRONZE',
+    alias = 'walmart_fact_table'
 ) }}
-
 
 SELECT
-   store_id,
-   dept_id,
-   store_date,
-   store_weekly_sales,
-   fuel_price,
-   store_temperature,
-   unemployment,
-   cpi,
-   markdown1,
-   markdown2,
-   markdown3,
-   markdown4,
-   markdown5,
-   insert_date,
-   update_date,
-   dbt_valid_from AS vrsn_start_date,
-   dbt_valid_to AS vrsn_end_date
+    store_id,
+    dept_id,
+    store_date,
+    store_weekly_sales,
+    fuel_price,
+    store_temperature,
+    unemployment,
+    cpi,
+    markdown1,
+    markdown2,
+    markdown3,
+    markdown4,
+    markdown5,
+    insert_date,
+    update_date,
+    dbt_valid_from  AS vrsn_start_date,
+    dbt_valid_to    AS vrsn_end_date
 FROM WALMART.SNAPSHOTS.walmart_fact_table_snapshot
 WHERE dbt_valid_to IS NULL

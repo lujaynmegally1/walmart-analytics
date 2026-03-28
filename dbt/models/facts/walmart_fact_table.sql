a4601ceb16a8cc0a169d53acb0c5b89dce343c14
lujaynmegally1 <97066492+lujaynmegally1@users.noreply.github.com>
2026-03-28 00:37:30 -0700

    Update walmart_fact_table.sql

--- dbt/models/facts/walmart_fact_table.sql
+++ dbt/models/facts/walmart_fact_table.sql
@@ -1,47 +1,38 @@
-{% snapshot walmart_fact_table_snapshot %}
-{{
-   config(
-     target_database='WALMART',
-     target_schema='SNAPSHOTS',
-     unique_key=['store_id', 'dept_id','store_date'],
-     strategy='check',
-     check_cols=[
-       'store_weekly_sales',
-       'fuel_price',
-       'store_temperature',
-       'unemployment',
-       'cpi',
-       'markdown1',
-       'markdown2',
-       'markdown3',
-       'markdown4',
-       'markdown5']
-   )
-}}
-WITH source AS (
-   SELECT
-       f.store_date,
-       f.store_id,
-       d.dept_id,
-       d.weekly_sales AS store_weekly_sales,
-       f.fuel_price,
-       f.temperature AS store_temperature,
-       f.unemployment,
-       f.cpi,
-       f.markdown1,
-       f.markdown2,
-       f.markdown3,
-       f.markdown4,
-       f.markdown5,
-       CURRENT_TIMESTAMP() AS insert_date,
-       CURRENT_TIMESTAMP() AS update_date
-   FROM WALMART.PUBLIC_RAW.fact_raw f
-   JOIN WALMART.PUBLIC_RAW.department_raw d
-   ON f.store_id = d.store_id
-   AND f.store_date = d.store_date
-)
-SELECT *
-FROM source
-{% endsnapshot %}
+{{ config(
+  materialized='incremental',
+  incremental_strategy = 'merge',
+  unique_key= ['store_id', 'dept_id'],
+  database='WALMART',
+  schema='BRONZE',
+  alias='walmart_store_dim'
+) }}
 
--- When a change is made in weekly sales, it overwrites that value to all other store_date and store id columns 
+
+{{ config(
+   materialized='table',
+   database='WALMART',
+   schema='BRONZE',
+   alias='walmart_fact_table'
+) }}
+
+
+SELECT
+   store_id,
+   dept_id,
+   store_date,
+   store_weekly_sales,
+   fuel_price,
+   store_temperature,
+   unemployment,
+   cpi,
+   markdown1,
+   markdown2,
+   markdown3,
+   markdown4,
+   markdown5,
+   insert_date,
+   update_date,
+   dbt_valid_from AS vrsn_start_date,
+   dbt_valid_to AS vrsn_end_date
+FROM WALMART.SNAPSHOTS.walmart_fact_table_snapshot
+WHERE dbt_valid_to IS NULL
